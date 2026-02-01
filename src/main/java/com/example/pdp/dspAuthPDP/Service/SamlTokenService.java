a8cb468b7bd7b20ab2045cbf5da86d50f1154c86
subhammukherjee <subhammukherjee911@gmail.com>
2026-02-01 19:31:08 +0530

    PE - SAML updated

--- src/main/java/com/example/pdp/dspAuthPDP/Service/SamlTokenService.java
+++ src/main/java/com/example/pdp/dspAuthPDP/Service/SamlTokenService.java
@@ -1,103 +1,148 @@
-package com.example.pdp.dspAuthPDP.Service;//package com.example.pdp.Service;
-//
-//import jakarta.annotation.PostConstruct;
-//import org.opensaml.core.xml.XMLObjectBuilderFactory;
-//import org.opensaml.core.xml.config.XMLObjectProviderRegistrySupport;
-//import org.opensaml.core.config.InitializationService;
-//import org.opensaml.core.xml.schema.XSString;
-//import org.opensaml.saml.saml2.core.*;
-//import org.springframework.stereotype.Service;
-//
-//
-//import javax.xml.namespace.QName;
-//import java.time.Instant;
-//import java.util.UUID;
-//
-//@Service
-//public class SamlTokenService {
-//
-//    private XMLObjectBuilderFactory builderFactory;
-//
-//    @PostConstruct
-//    public void init() throws Exception {
-//        InitializationService.initialize();
-//        builderFactory = XMLObjectProviderRegistrySupport.getBuilderFactory();
-//    }
-//
-//    public Assertion createSamlAssertion(
-//            String tokenId,
-//            String authLevel,
-//            String customerId,
-//            String sessionCorrelationId,
-//            String accountType,
-//            String channel
-//    ) {
-//
-//        // ---- ASSERTION ----
-//        Assertion assertion = build(Assertion.DEFAULT_ELEMENT_NAME);
-//        assertion.setID("_" + UUID.randomUUID());
-//        assertion.setIssueInstant(Instant.now());
-//
-//        Issuer issuer = build(Issuer.DEFAULT_ELEMENT_NAME);
-//        issuer.setValue("https://banking.example.com/idp");
-//        assertion.setIssuer(issuer);
-//
-//        // ---- SUBJECT ----
-//        NameID nameID = build(NameID.DEFAULT_ELEMENT_NAME);
-//        nameID.setValue(customerId);
-//        nameID.setFormat(NameIDType.PERSISTENT);
-//
-//        Subject subject = build(Subject.DEFAULT_ELEMENT_NAME);
-//        subject.setNameID(nameID);
-//        assertion.setSubject(subject);
-//
-//        // ---- AUTHN STATEMENT ----
-//        AuthnStatement authnStatement = build(AuthnStatement.DEFAULT_ELEMENT_NAME);
-//        authnStatement.setAuthnInstant(Instant.now());
-//
-//        AuthnContext authnContext = build(AuthnContext.DEFAULT_ELEMENT_NAME);
-//        AuthnContextClassRef classRef =
-//                build(AuthnContextClassRef.DEFAULT_ELEMENT_NAME);
-//        classRef.setURI(authLevel);
-//
-//        authnContext.setAuthnContextClassRef(classRef);
-//        authnStatement.setAuthnContext(authnContext);
-//        assertion.getAuthnStatements().add(authnStatement);
-//
-//        // ---- ATTRIBUTES ----
-//        AttributeStatement attributeStatement =
-//                build(AttributeStatement.DEFAULT_ELEMENT_NAME);
-//
-//        attributeStatement.getAttributes().add(
-//                createAttribute("sessionCorrelationId", sessionCorrelationId));
-//        attributeStatement.getAttributes().add(
-//                createAttribute("serviceId", customerId));
-//        attributeStatement.getAttributes().add(
-//                createAttribute("accountType", accountType));
-//
-//        assertion.getAttributeStatements().add(attributeStatement);
-//
-//        return assertion;
-//    }
-//
-//    // ---------- HELPERS ----------
-//
-//    private Attribute createAttribute(String name, String value) {
-//        Attribute attribute = build(Attribute.DEFAULT_ELEMENT_NAME);
-//        attribute.setName(name);
-//
-//        XSString attrValue = (XSString) builderFactory
-//                .getBuilder(XSString.TYPE_NAME)
-//                .buildObject(AttributeValue.DEFAULT_ELEMENT_NAME, XSString.TYPE_NAME);
-//
-//        attrValue.setValue(value);
-//        attribute.getAttributeValues().add(attrValue);
-//
-//        return attribute;
-//    }
-//
-//    @SuppressWarnings("unchecked")
-//    private <T> T build(QName qName) {
-//        return (T) builderFactory.getBuilder(qName).buildObject(qName);
-//    }
-//}
+package com.example.pdp.dspAuthPDP.Service;
+
+import org.springframework.stereotype.Service;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringWriter;
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+public class SamlTokenService {
+
+    private static final String SAML_NS =
+            "urn:oasis:names:tc:SAML:2.0:assertion";
+
+    public String createSamlAssertion(
+            String tokenId,
+            String authLevel,
+            String customerId,
+            String sessionCorrelationId,
+            String accountType,
+            String channel
+    ) {
+        try {
+            String assertionId = "_" + UUID.randomUUID();
+            String issueInstant = Instant.now().toString();
+
+            Document doc = DocumentBuilderFactory
+                    .newInstance()
+                    .newDocumentBuilder()
+                    .newDocument();
+
+            // ----- Assertion -----
+            Element assertion = doc.createElementNS(SAML_NS, "saml2:Assertion");
+            assertion.setAttribute("ID", assertionId);
+            assertion.setAttribute("Version", "2.0");
+            assertion.setAttribute("IssueInstant", issueInstant);
+            doc.appendChild(assertion);
+
+            // ----- Issuer -----
+            Element issuer = doc.createElementNS(SAML_NS, "saml2:Issuer");
+            issuer.setTextContent("https://www.google.com/");
+            assertion.appendChild(issuer);
+
+            // ----- Subject -----
+            Element subject = doc.createElementNS(SAML_NS, "saml2:Subject");
+            Element nameId = doc.createElementNS(SAML_NS, "saml2:NameID");
+            nameId.setAttribute(
+                    "Format",
+                    "urn:oasis:names:tc:SAML:2.0:nameid-format:persistent"
+            );
+            nameId.setTextContent(customerId);
+            subject.appendChild(nameId);
+            assertion.appendChild(subject);
+
+            // ----- AuthnStatement -----
+            Element authnStatement =
+                    doc.createElementNS(SAML_NS, "saml2:AuthnStatement");
+            authnStatement.setAttribute("AuthnInstant", issueInstant);
+
+            Element authnContext =
+                    doc.createElementNS(SAML_NS, "saml2:AuthnContext");
+            Element classRef =
+                    doc.createElementNS(
+                            SAML_NS,
+                            "saml2:AuthnContextClassRef"
+                    );
+            classRef.setTextContent(authLevel);
+
+            authnContext.appendChild(classRef);
+            authnStatement.appendChild(authnContext);
+            assertion.appendChild(authnStatement);
+
+            // ----- AttributeStatement -----
+            Element attrStatement =
+                    doc.createElementNS(
+                            SAML_NS,
+                            "saml2:AttributeStatement"
+                    );
+
+            addAttribute(doc, attrStatement,
+                    "sessionCorrelationId", sessionCorrelationId);
+            addAttribute(doc, attrStatement,
+                    "serviceId", customerId);
+            addAttribute(doc, attrStatement,
+                    "accountType", accountType);
+            addAttribute(doc, attrStatement,
+                    "channel", channel);
+            addAttribute(doc, attrStatement,
+                    "tokenId", tokenId);
+            addAttribute(doc, attrStatement,
+                    "authLevel", authLevel);
+
+            assertion.appendChild(attrStatement);
+
+            // ----- Serialize XML -----
+            Transformer transformer =
+                    TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(
+                    OutputKeys.OMIT_XML_DECLARATION,
+                    "yes"
+            );
+            transformer.setOutputProperty(
+                    OutputKeys.INDENT,
+                    "yes"
+            );
+
+            StringWriter writer = new StringWriter();
+            transformer.transform(
+                    new DOMSource(doc),
+                    new StreamResult(writer)
+            );
+
+            return writer.toString();
+
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Failed to generate SAML Assertion",
+                    e
+            );
+        }
+    }
+
+    private void addAttribute(
+            Document doc,
+            Element statement,
+            String name,
+            String value
+    ) {
+        Element attribute =
+                doc.createElementNS(SAML_NS, "saml2:Attribute");
+        attribute.setAttribute("Name", name);
+
+        Element attrValue =
+                doc.createElementNS(SAML_NS, "saml2:AttributeValue");
+        attrValue.setTextContent(value);
+
+        attribute.appendChild(attrValue);
+        statement.appendChild(attribute);
+    }
+}
